560d99fe16f50e588bfbb9b7fe7e30b3ad2f15de
swarnim992 <90375441+swarnim992@users.noreply.github.com>
2024-01-20 20:07:48 +0530

    Update tambola.cpp

--- tambola.cpp
+++ tambola.cpp
@@ -1,19 +1,11 @@
 #include<bits/stdc++.h>
-//#include<sqlite3ext.h>
-//#include <json/json.h> 
 
-
-using namespace std;
-#include "sqlite3.h"
-
-
+using namespace std;    
 
 vector<vector<int>> tk(3,vector<int>(9));
-int max_slots = 15;
-vector<vector<int>>  tb(3,vector<int>(9));
-//int i, j, k;
+//vector<vector<int>>  tb(3,vector<int>(9));
+vector<vector<vector<int>>> r;
 
-int ticket[3][9];
 int i, j, k;
 vector<int> aa={0,1,2,3,4,5,6,7,8};
 vector<vector<int>> nums={{1,2,3,4,5,6,7,8,9},{10,11,12,13,14,15,16,17,18,19},{20,21,22,23,24,25,26,27,28,29},
@@ -21,19 +13,8 @@ vector<vector<int>> nums={{1,2,3,4,5,6,7,8,9},{10,11,12,13,14,15,16,17,18,19},{2
                     {60,61,62,63,64,65,66,67,68,69},{70,71,72,73,74,75,76,77,78,79},{80,81,82,83,84,85,86,87,88,89,90}};
 
 
-void fillRandomly() {
-    int filled = 0;
-    for (i = 0; i < 9; i++) {
-        // keeping 1 number in column (base case)
-        j = rand() % 3;
-        tb[j][i] = 1;
-        filled++;
-    }
 
-    
-}
-
-void fillRandomly1() {
+void fillr() {
     int c=0;
     int check=0;
 
@@ -73,13 +54,11 @@ void orderTicket() {
     int temp = 0;
     for (i = 0; i < 9; i++) {
         for (j = 0; j < 3; j++) {
-            // Using Bubble sort to sort columns
+
             if (tk[j][i] != 0) {
                 for (k = j; k < 3; k++) {
                     if (tk[k][i] < tk[j][i] && tk[k][i] != 0) {
-                        temp = tk[j][i];
-                        tk[j][i] = tk[k][i];
-                        tk[k][i] = temp;
+                        swap(tk[j][i],tk[k][i]);
                     }
                 }
             }
@@ -87,26 +66,9 @@ void orderTicket() {
     }
 }
 
-void removeRedundancy() {
-    for (i = 0; i < 9; i++) {
-        for (j = 0; j < 3; j++) {
-            if (tk[j][i] != 0) {
-                for (k = j + 1; k < 3; k++) {
-                    if (tk[k][i] != 0) {
-                        while (tk[k][i] == tk[j][i]) {
-                            tk[j][i] = tb[j][i] * (rand() % (10) + (10 * i));
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-void clearPrev() {
+void clearTk() {
     for (i = 0; i < 3; i++) {
         for (j = 0; j < 9; j++) {
-            tb[i][j] = 0;
             tk[i][j] = 0;
         }
     }
@@ -123,68 +85,65 @@ void printTicket(vector<vector<int>> mat) {
 }
 
 void generateTicket() {
-    //fillRandomly();
+    fillr();
     //cout<<2<<endl;
-    int c=0;
 
-    fillRandomly1();
-    //cout<<3<<endl;
-    /*
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 9; j++) {
-
-            //k=nums[j][i];
-            if(nums[j].size()>0)
-            c=rand() % (nums[j].size());
-            else c=-1;
-
-            if(nums[j].size()>0){
-
-                int temp= tb[i][j] * nums[j][c];
-
-                cout<<temp<<' ';
-                nums[j].erase(nums[j].begin()+c);
-
-                //int temp=tb[i][j] * (rand() % (10) + (10 * j));
-                
-                //if(temp)
-                tk[i][j] = temp;
-                //else ticket[i][j]=temp+1;
-            }
-        }
-    }
-    */
-    //cout<<4<<endl;
-
-    
-   // removeRedundancy();
     orderTicket();
-    printTicket(tk);
-    clearPrev();
+
+    r.push_back(tk);
+
+    //printTicket(tk);
+    clearTk();
     
 }
 
 int main(){
 
     int n;
-    //cin>>n;
 
-    sqlite3 *db;
-    int rc = sqlite3_open("./tick.db", &db);
-    if (rc != SQLITE_OK) {
-        std::cerr << "Cannot open database: " << sqlite3_errmsg(db) << std::endl;
-        sqlite3_close(db);
-        return rc;
-    }
+    cout<<"Enter The number of tickets you want to Make (N) : ";
+    cin>>n;
 
-    vector<vector<vector<int>>> r(n);
-    //srand(time(0));
+    cout<<endl;
 
-    n=6;
     while(n--){
-        //cout<<1<<endl;
         generateTicket();
     }
+
+    cout<<"------------Tickets created succesfully-------------\n";
+    cout<<"----------------------------------------------------"<<endl;
+    cout<<endl;
+
+    int ck;
+    cout<<endl;
+
+    while(1){
+        cout<<"Enter the number of ticket you want to view or enter 0 to view all : ";
+        cin>>ck;
+
+        if(ck!=0){
+            cout<<endl;
+            cout<<"T"<<ck<<':'<<endl;
+            
+            printTicket(r[ck-1]);
+        }
+        else{
+            cout<<endl;
+            int count=1;
+            for(auto res : r){
+                cout<<"T"<<count++<<':'<<endl;                
+                printTicket(res);
+                
+            }
+        }
+
+        cout<<"Enter 1 to continue else 0 : ";
+        bool ch;
+        cin>>ch;
+
+        if(!ch) break;
+    }
+    
 
     return 0;
 }
